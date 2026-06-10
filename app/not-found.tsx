eaf0f14917f179f499075d316a13b6e1f3b931aa
Stefano Taillefert <stefano.taille@gmail.com>
2026-06-10 11:57:51 +0200

    Add padding to loading page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,12 @@ import BackButton from "./components/BackButton";
 
 export default function NotFoundPage() {
   return (
-    <>
-      <div className="text-2xl">Error 404</div>
-      <p className="mb-4">
+    <div className="p-12">
+      <div className="text-2xl mb-4">Error 404</div>
+      <p className="mb-8">
         Not sure what you&apos;re looking for, but it&apos;s not here.
       </p>
       <BackButton href="/" />
-    </>
+    </div>
   );
 }
